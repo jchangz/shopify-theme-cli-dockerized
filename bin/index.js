df0d724f0e2ea562c77014b7dd6410c07c8907cf
Jian Chang <45157140+jchangz@users.noreply.github.com>
2023-05-20 02:02:38 -0600

    feat(bin): allow install in current directory or named directory

--- bin/index.js
+++ bin/index.js
@@ -9,40 +9,36 @@ import ora from "ora"
 
 const chalkError = (err) => `${chalk.white.bgRed.bold("ERROR")} ${err}`
 
-if (process.argv.length < 3) {
-  console.log(
-    chalkError("You have to provide a name for the installation directory")
-  )
-  console.log("For example: npx shopify-theme-cli-dockerized my-app")
-  process.exit(1)
-}
-
-const folderPath = process.argv[2]
-
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const __parentdir = path.join(__dirname, "..")
 const __devcontainer = path.join(__parentdir, ".devcontainer")
 
 const __installdir = process.cwd()
-const __devcontainerinstalldir = path.join(
-  __installdir,
-  folderPath,
-  ".devcontainer"
-)
+let __devcontainerinstalldir = path.join(__installdir, ".devcontainer")
 
-try {
-  fs.mkdirSync(folderPath)
-} catch (err) {
-  if (err.code === "EEXIST") {
-    console.log(
-      chalkError(
-        `The folder "${folderPath}" already exists in the current directory, please give it another name.`
+if (process.argv.length > 2) {
+  const folderPath = process.argv[2]
+
+  try {
+    fs.mkdirSync(folderPath)
+  } catch (err) {
+    if (err.code === "EEXIST") {
+      console.log(
+        chalkError(
+          `The folder "${folderPath}" already exists in the current directory, please give it another name.`
+        )
       )
-    )
-  } else {
-    console.log(chalkError(err))
+    } else {
+      console.log(chalkError(err))
+    }
+    process.exit(1)
   }
-  process.exit(1)
+
+  __devcontainerinstalldir = path.join(
+    __installdir,
+    folderPath,
+    ".devcontainer"
+  )
 }
 
 const spinner = ora().start("Copying .devcontainer files")
