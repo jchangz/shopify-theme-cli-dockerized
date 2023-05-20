0c1b4391d0cbe29f3599ea8c982094629da1c579
Jian Chang <45157140+jchangz@users.noreply.github.com>
2023-05-19 23:56:13 -0600

    refactor(bin): require name for installation directory

--- bin/index.js
+++ bin/index.js
@@ -9,12 +9,41 @@ import ora from "ora"
 
 const chalkError = (err) => `${chalk.white.bgRed.bold("ERROR")} ${err}`
 
+if (process.argv.length < 3) {
+  console.log(
+    chalkError("You have to provide a name for the installation directory")
+  )
+  console.log("For example: npx shopify-theme-cli-dockerized my-app")
+  process.exit(1)
+}
+
+const folderPath = process.argv[2]
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const __parentdir = path.join(__dirname, "..")
 const __devcontainer = path.join(__parentdir, ".devcontainer")
 
 const __installdir = process.cwd()
-const __devcontainerinstalldir = path.join(__installdir, ".devcontainer")
+const __devcontainerinstalldir = path.join(
+  __installdir,
+  folderPath,
+  ".devcontainer"
+)
+
+try {
+  fs.mkdirSync(folderPath)
+} catch (err) {
+  if (err.code === "EEXIST") {
+    console.log(
+      chalkError(
+        `The folder "${folderPath}" already exists in the current directory, please give it another name.`
+      )
+    )
+  } else {
+    console.log(chalkError(err))
+  }
+  process.exit(1)
+}
 
 const spinner = ora().start("Copying .devcontainer files")
 const delay = 500
