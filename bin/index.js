c9fdae8b4789d757bc9ad7e86630240816b306b6
Jian Chang <45157140+jchangz@users.noreply.github.com>
2023-08-23 22:50:36 -0600

    refactor(bin): use @inquirer/prompts

--- bin/index.js
+++ bin/index.js
@@ -5,7 +5,7 @@ import path from "path"
 import { fileURLToPath } from "url"
 import { setTimeout } from "timers/promises"
 import ora from "ora"
-import inquirer from "inquirer"
+import { select, input } from "@inquirer/prompts"
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const __parentdir = path.join(__dirname, "..")
@@ -15,59 +15,53 @@ const __installdir = process.cwd()
 let __devcontainerinstalldir = path.join(__installdir, ".devcontainer")
 let __folderpathname
 
-const q1 = await inquirer
-  .prompt([
+const installType = await select({
+  message: "Choose your installation directory",
+  choices: [
     {
-      name: "installType",
-      message: "Choose your installation directory",
-      type: "list",
-      choices: ["Current directory", "New named folder"],
+      name: "Current directory",
+      value: "current",
+      description: "Create files in this directory",
     },
-  ])
-  .then((answer) => {
-    const { installType } = answer
-    if (installType === "New named folder") return true
-    return false
-  })
+    {
+      name: "New folder",
+      value: "new",
+      description: "Create files in a new named folder in this directory",
+    },
+  ],
+})
 
-if (q1) {
+if (installType === "new") {
   while (!__folderpathname) {
-    await inquirer
-      .prompt([
-        {
-          name: "folderPath",
-          type: "input",
-          message: "Your directory name?",
-          default: "my-app",
-        },
-      ])
-      .then((answer) => {
-        const { folderPath } = answer
-        try {
-          fs.mkdirSync(folderPath)
-          __folderpathname = folderPath
-        } catch (err) {
-          if (err.code === "EEXIST") {
-            console.log(
-              `The folder "${folderPath}" already exists in the current directory, please give it another name`
-            )
-          } else {
-            console.log(err)
-          }
+    await input({
+      message: "Enter your folder name",
+      default: "my-app",
+    }).then((folderPath) => {
+      try {
+        fs.mkdirSync(folderPath)
+        __folderpathname = folderPath
+      } catch (err) {
+        if (err.code === "EEXIST") {
+          console.log(
+            `The folder "${folderPath}" already exists in the current directory, please try another name`
+          )
+        } else {
+          console.log(err)
         }
-        __devcontainerinstalldir = path.join(
-          __installdir,
-          folderPath,
-          ".devcontainer"
-        )
-      })
+      }
+      __devcontainerinstalldir = path.join(
+        __installdir,
+        folderPath,
+        ".devcontainer"
+      )
+    })
   }
 }
 
 const spinner = ora().start("Copying .devcontainer files")
 
 async function copyFolderSync(from, to, spinner) {
-  const delay = 500
+  const delay = 250
 
   try {
     fs.mkdirSync(to)
@@ -82,7 +76,7 @@ async function copyFolderSync(from, to, spinner) {
     if (err.code === "EEXIST") {
       await setTimeout(delay)
       spinner.fail(
-        'The directory ".devcontainer" already exists, please remove it'
+        'The directory ".devcontainer" already exists, please remove it before installing'
       )
     } else {
       spinner.fail(err)
